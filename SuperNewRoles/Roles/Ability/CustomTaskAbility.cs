39e0c49118acd9ef2dfe16cc03f5b6e6d9f78131
Aisu7 <soma12ito03@gmail.com>
2026-07-07 17:54:46 +0900

    Refactor CustomTaskAbility and related patches

--- SuperNewRoles/Roles/Ability/CustomTaskAbility.cs
+++ SuperNewRoles/Roles/Ability/CustomTaskAbility.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using SuperNewRoles.Modules;
-using SuperNewRoles.Events;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -46,7 +45,7 @@ public class CustomTaskTypeAbility : AbilityBase
     {
         base.AttachToLocalPlayer();
         // 先に読み込んでおく
-        CustomTaskTypePatches.GetTargetShip(TargetMap, (_) => { });
+        CustomTaskTypePatches.ConsolePatch.GetTargetShip(TargetMap, (_) => { });
     }
 
     public void AssignTasks(int count)
@@ -69,135 +68,132 @@ public class CustomTaskTypeAbility : AbilityBase
 [HarmonyPatch]
 public static class CustomTaskTypePatches
 {
-    // Console.Use と MapConsole.Use の共通処理を一か所にまとめる。
-    // FixWeatherNode（気象ノード）など一部タスクは Console ではなく
-    // MapConsole コンポーネントを使うため、両方のパッチが必要。
-    private static Minigame _preMinigame;
-
-    private static void SharedPrefix(PlayerTask task, bool canUse)
-    {
-        if (!canUse || task == null) return;
-
-        var customTaskTypeAbility = ExPlayerControl.LocalPlayer.GetAbility<CustomTaskTypeAbility>();
-        if (customTaskTypeAbility == null) return;
-        if (!customTaskTypeAbility.ShouldChangeTask()) return;
-
-        // サボタージュタスクは変換しない。
-        // ただし TaskType だけで判定すると、ポーラスの研究室スイッチ（ResetSeismic）のように
-        // 通常タスクとサボタージュが同じコンソールを共有するケースで
-        // 通常タスク時でも変換されなくなってしまう。
-        // そのため「実際にサボタージュが発動中かどうか」も合わせて確認する。
-        bool isSabotageTask = task.TaskType is TaskTypes.FixLights or TaskTypes.RestoreOxy or
-            TaskTypes.ResetReactor or TaskTypes.ResetSeismic or TaskTypes.FixComms or
-            TaskTypes.StopCharles or TaskTypes.MushroomMixupSabotage;
-
-        if (isSabotageTask)
-        {
-            bool sabotageActive = SaboStateTracker.activeSaboTypes.Count > 0;
-            if (sabotageActive || task.TaskType is not TaskTypes.ResetSeismic)
-                return;
-        }
-
-        _preMinigame = task.MinigamePrefab;
-        GetTargetShip(customTaskTypeAbility.TargetMap, (ship) =>
-        {
-            var targetTask = GetTargetTaskFromShip(ship, customTaskTypeAbility.TargetTaskType);
-            if (targetTask != null)
-                task.MinigamePrefab = targetTask.MinigamePrefab;
-        });
-    }
-
-    private static void SharedPostfix(PlayerTask task, bool canUse)
-    {
-        if (!canUse || task == null) return;
-
-        var customTaskTypeAbility = ExPlayerControl.LocalPlayer.GetAbility<CustomTaskTypeAbility>();
-        if (customTaskTypeAbility == null) return;
-        if (!customTaskTypeAbility.ShouldChangeTask()) return;
-
-        if (_preMinigame != null)
-        {
-            task.MinigamePrefab = _preMinigame;
-            _preMinigame = null;
-        }
-    }
-
-    // 通常コンソール（大多数のタスク）
     [HarmonyPatch(typeof(Console), nameof(Console.Use))]
     public static class ConsolePatch
     {
+        private static Minigame preMinigame;
+
         static void Prefix(Console __instance)
         {
             __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
-            SharedPrefix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
+            HandlePrefix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
         }
 
         static void Postfix(Console __instance)
         {
             __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
-            SharedPostfix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
-        }
-    }
-
-    // MapConsole（FixWeatherNode など一部タスクが使うコンソール種別）
-    [HarmonyPatch(typeof(MapConsole), nameof(MapConsole.Use))]
-    public static class MapConsolePatch
-    {
-        static void Prefix(MapConsole __instance)
-        {
-            __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
-            SharedPrefix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
+            HandlePostfix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
         }
 
-        static void Postfix(MapConsole __instance)
+        // MapConsole.Use パッチから呼び出せるよう internal static に切り出す
+        internal static void HandlePrefix(PlayerTask task, bool canUse)
         {
-            __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
-            SharedPostfix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
-        }
-    }
+            var customTaskTypeAbility = ExPlayerControl.LocalPlayer.GetAbility<CustomTaskTypeAbility>();
+            if (customTaskTypeAbility == null) return;
+            if (!customTaskTypeAbility.ShouldChangeTask()) return;
+            if (!canUse) return;
+            if (task == null) return;
+            if (task.TaskType is TaskTypes.FixLights or TaskTypes.RestoreOxy or TaskTypes.ResetReactor or
+                TaskTypes.ResetSeismic or TaskTypes.FixComms or TaskTypes.StopCharles or TaskTypes.MushroomMixupSabotage)
+                return;
 
-    public static void GetTargetShip(MapNames? targetMap, Action<ShipStatus> onLoaded)
-    {
-        if (targetMap.HasValue)
-        {
-            switch (targetMap.Value)
+            preMinigame = task.MinigamePrefab;
+            GetTargetShip(customTaskTypeAbility.TargetMap, (ship) =>
             {
-                case MapNames.Fungle:
-                    if (GameOptionsManager.Instance.CurrentGameOptions.MapId == (int)MapNames.Fungle)
-                    {
-                        onLoaded(ShipStatus.Instance);
-                        return;
-                    }
-                    Logger.Info("LoadMap: Fungle");
-                    MapLoader.LoadMap(MapNames.Fungle, (ship) => { onLoaded(ship); });
-                    break;
-                case MapNames.Airship:
-                    if (GameOptionsManager.Instance.CurrentGameOptions.MapId == (int)MapNames.Airship)
-                    {
-                        onLoaded(ShipStatus.Instance);
-                        return;
-                    }
-                    Logger.Info("LoadMap: Airship");
-                    MapLoader.LoadMap(MapNames.Airship, (ship) => { onLoaded(ship); });
-                    break;
-                default:
-                    onLoaded(ShipStatus.Instance);
-                    break;
+                var targetTask = GetTargetTaskFromShip(ship, customTaskTypeAbility.TargetTaskType);
+                if (targetTask != null)
+                    task.MinigamePrefab = targetTask.MinigamePrefab;
+            });
+        }
+
+        internal static void HandlePostfix(PlayerTask task, bool canUse)
+        {
+            var customTaskTypeAbility = ExPlayerControl.LocalPlayer.GetAbility<CustomTaskTypeAbility>();
+            if (customTaskTypeAbility == null) return;
+            if (!customTaskTypeAbility.ShouldChangeTask()) return;
+            if (!canUse) return;
+            if (task == null) return;
+            if (preMinigame != null)
+            {
+                task.MinigamePrefab = preMinigame;
+                preMinigame = null;
             }
         }
 
-        // 現在のマップから取得
-        onLoaded(ShipStatus.Instance);
+        public static void GetTargetShip(MapNames? targetMap, Action<ShipStatus> onLoaded)
+        {
+            // 特定のマップが指定されている場合はそのマップから取得
+            if (targetMap.HasValue)
+            {
+                switch (targetMap.Value)
+                {
+                    case MapNames.Fungle:
+                        if (GameOptionsManager.Instance.CurrentGameOptions.MapId == (int)MapNames.Fungle)
+                        {
+                            onLoaded(ShipStatus.Instance);
+                            return;
+                        }
+                        Logger.Info("LoadMap: Fungle");
+                        MapLoader.LoadMap(MapNames.Fungle, (ship) =>
+                        {
+                            onLoaded(ship);
+                        });
+                        break;
+                    case MapNames.Airship:
+                        if (GameOptionsManager.Instance.CurrentGameOptions.MapId == (int)MapNames.Airship)
+                        {
+                            onLoaded(ShipStatus.Instance);
+                            return;
+                        }
+                        Logger.Info("LoadMap: Airship");
+                        MapLoader.LoadMap(MapNames.Airship, (ship) =>
+                        {
+                            onLoaded(ship);
+                        });
+                        break;
+                    default:
+                        onLoaded(ShipStatus.Instance);
+                        break;
+                }
+            }
+
+            // 現在のマップから取得
+            onLoaded(ShipStatus.Instance);
+        }
+
+        private static NormalPlayerTask GetTargetTaskFromShip(ShipStatus ship, TaskTypes targetTaskType)
+        {
+            // ショートタスクから探す
+            var shortTask = ship.ShortTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
+            if (shortTask != null) return shortTask;
+
+            // ロングタスクから探す
+            var longTask = ship.LongTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
+            if (longTask != null) return longTask;
+
+            // コモンタスクから探す
+            var commonTask = ship.CommonTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
+            if (commonTask != null) return commonTask;
+
+            return null;
+        }
+    }
+}
+
+// ポーラス（The Fungle）の気象ノードは MapConsole クラスを使用しているため
+// Console.Use パッチが発火しない。同じ処理を MapConsole.Use にも追加する。
+[HarmonyPatch(typeof(MapConsole), nameof(MapConsole.Use))]
+public static class MapConsoleCustomTaskTypePatch
+{
+    static void Prefix(MapConsole __instance)
+    {
+        __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
+        CustomTaskTypePatches.ConsolePatch.HandlePrefix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
     }
 
-    private static NormalPlayerTask GetTargetTaskFromShip(ShipStatus ship, TaskTypes targetTaskType)
+    static void Postfix(MapConsole __instance)
     {
-        var shortTask = ship.ShortTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
-        if (shortTask != null) return shortTask;
-        var longTask = ship.LongTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
-        if (longTask != null) return longTask;
-        var commonTask = ship.CommonTasks.FirstOrDefault(x => x.TaskType == targetTaskType);
-        if (commonTask != null) return commonTask;
-        return null;
+        __instance.CanUse(PlayerControl.LocalPlayer.Data, out bool canUse, out bool _);
+        CustomTaskTypePatches.ConsolePatch.HandlePostfix(__instance.FindTask(PlayerControl.LocalPlayer), canUse);
     }
 }
